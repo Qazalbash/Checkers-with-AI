4521ea891b65b8a90a449008d457c8abf8cc59ef
Meesum A. Qazalbash <mesumali26.ma@gmail.com>
2022-07-03 22:04:24 +0500

    Computer vs Computer done!

--- checker.py
+++ checker.py
@@ -4,7 +4,8 @@ import time
 from typing import Optional
 
 
-class Checker(object):
+class Checker:
+    """class to simulate checkers game where computer play against computer"""
     L1_norm = lambda x, y: abs(x[0] - y[0]) + abs(x[1] - y[1])
     normal_hops = {(1, 1), (-1, -1), (1, -1), (-1, 1)}
     count = {'b': 24, 'w': 24}
@@ -18,11 +19,21 @@ class Checker(object):
         'h': '🟩'
     }
 
-    def __init__(self) -> None:
+    def __init__(self, sleep: float = 0.0, clear_screen: bool = True) -> None:
+        """Constructor
+
+        Args:
+            sleep (float, optional): number seconds to Sleep. Defaults to 0.
+            clear_screen (bool, optional): if True then clear screen after each new move. Defaults to True.
+        """
+        self.sleep = sleep
+        self.clear_screen = clear_screen
         self.last_move = set()
         self.initiate_peices()
+        self.parity = {'w': {0: True, 1: True}, 'b': {0: True, 1: True}}
 
     def initiate_peices(self) -> None:
+        """places the peices on the board"""
         self.peice = dict()
         for c in range(8):
             for r in range(3):
@@ -32,11 +43,13 @@ class Checker(object):
             for r in range(5, 8):
                 self.peice[(r, c)] = 'w'
 
-    def draw_board(self) -> None:
-        # time.sleep(0.5)
-        os.system('clear')
-        k = 'r'
+    def print_board(self) -> None:
+        """print the board on the terminal"""
+        time.sleep(self.sleep)
+        if self.clear_screen:
+            os.system('clear')
 
+        k = 'r'
         for i in range(8):
             for j in range(7):
                 k = 'y' if (i + j) % 2 else 'r'
@@ -53,6 +66,12 @@ class Checker(object):
             print(self.tile[p])
 
     def move(self, pos: Optional[tuple[int, int]], side: str) -> None:
+        """moves the peice according to the sequence of position given to
+
+        Args:
+            pos (Optional[tuple[int, int]]): sequence of position to move peice on the board
+            side (str): peice is from which side
+        """
 
         self.last_move = set()
         opp_side = 'b' if side == 'w' else 'w'
@@ -92,6 +111,15 @@ class Checker(object):
 
     @staticmethod
     def direction(dr: int, dc: int) -> str:
+        """returns the direction in which the change is happening
+
+        Args:
+            dr (int): change in row
+            dc (int): change in column
+
+        Returns:
+            str: the direction string
+        """
         return ('dl' * (dc < 0) + 'dr' *
                 (dc > 0)) * (dr > 0) + ('ul' * (dc < 0) + 'ur' *
                                         (dc > 0)) * (dr < 0)
@@ -99,19 +127,35 @@ class Checker(object):
     def hops(self,
              r: int,
              c: int,
-             p: str,
-             path: dict,
-             visited: set,
+             path: dict = dict(),
+             visited: set = set(),
              first: bool = True) -> dict:
+        """returns all the hops than can happen from a given position.
 
+        Args:
+            r (int): row of the peice
+            c (int): column of the peice
+            path (dict, optional): container storing the path. Defaults to dict().
+            visited (set, optional): visited position. Defaults to set().
+            first (bool, optional): True if the hop is first hop. Defaults to True.
+
+        Returns:
+            dict: container that contains all the paths
+        """
+
+        p = self.peice[(r, c)]
         if p == 'b':
             hops = {(1, 1), (1, -1)}
+            allowed = {'w'}
         elif p == 'w':
             hops = {(-1, 1), (-1, -1)}
+            allowed = {'b'}
         else:
             hops = self.normal_hops
-
-        op = 'w' if p.lower() == 'b' else 'b'
+            if p == 'B':
+                allowed = {'w', 'W'}
+            else:
+                allowed = {'b', 'B'}
 
         path['coordinate'] = (r, c)
         path['cost'] = path.get('cost', 0)
@@ -135,7 +179,7 @@ class Checker(object):
         for dr, dc in hops:
             if 0 <= r + 2 * dr < 8 and 0 <= c + 2 * dc < 8 and (
                     r + 2 * dr, c + 2 * dc) not in visited and self.peice[(
-                        r + dr, c + dc)].lower() == op and self.peice[(
+                        r + dr, c + dc)] in allowed and self.peice[(
                             r + 2 * dr, c + 2 * dc)] == 'o':
                 path[self.direction(dr, dc)] = {
                     'coordinate': (r + 2 * dr, c + 2 * dc),
@@ -146,11 +190,21 @@ class Checker(object):
                     'cost': 2
                 }
                 visited.add((r + 2 * dr, c + 2 * dc))
-                self.hops(r + 2 * dr, c + 2 * dc, p,
-                          path[self.direction(dr, dc)], visited, False)
+                self.hops(r + 2 * dr, c + 2 * dc, path[self.direction(dr, dc)],
+                          visited, False)
         return path
 
     def deepest_path(self, path: dict) -> Optional[tuple]:
+        """returns the deepest path in a tree, if there are more 
+        than one then select any one randomly
+
+        Args:
+            path (dict): tree in a form of nested dictionaries
+
+        Returns:
+            Optional[tuple]: list of all nodes that are on the 
+            deepest path in the tree
+        """
         if path is None:
             return []
 
@@ -160,11 +214,12 @@ class Checker(object):
         up_left = self.deepest_path(path['ul'])
 
         length = [len(down_right), len(down_left), len(up_right), len(up_left)]
+        deepest = max(length)
 
         index = [0, 1, 2, 3]
         i = random.choice(index)
 
-        while length[i] != max(length):
+        while length[i] != deepest:
             index.remove(i)
             i = random.choice(index)
 
@@ -180,7 +235,7 @@ class Checker(object):
         index = [0, 1, 2, 3]
         i = random.choice(index)
 
-        while length[i] != max(length):
+        while length[i] != deepest:
             index.remove(i)
             i = random.choice(index)
 
@@ -193,18 +248,39 @@ class Checker(object):
         if i == 3:
             return up_left
 
-    def possible_paths(self, side: str) -> Optional[dict]:
+    def paths(self, side: str) -> Optional[Optional[tuple[int, int]]]:
+        """returns the deepest path of all peices from side
+
+        Args:
+            side (str): the side to find the deepest path of its peices
+
+        Returns:
+            Optional[Optional[tuple[int,int]]]: list of a deepest 
+            path possible for each peice
+        """
+
         path = []
+        self.parity[side] = {0: False, 1: False}
         for r in range(8):
             for c in range(8):
                 if self.peice[(r, c)].lower() == side:
-                    path.append(
-                        self.deepest_path(
-                            self.hops(r, c, self.peice[(r, c)], {}, set())))
+                    self.parity[side][(r + c) %
+                                      2] = self.parity[side][(r + c) %
+                                                             2] or True
+                    path.append(self.deepest_path(self.hops(r, c)))
         return path
 
     def computer_move(self, side: str) -> Optional[tuple[int, int]]:
-        path = self.possible_paths(side)
+        """returns the move computer wanna play
+
+        Args:
+            side (str): side which computer playing
+
+        Returns:
+            Optional[tuple[int, int]]: list of the position to 
+            move peice on the board
+        """
+        path = self.paths(side)
         moves = {}
         jump = 0
         for k in path:
@@ -222,26 +298,34 @@ class Checker(object):
         return move[::-1]
 
     def start(self) -> None:
-        self.draw_board()
+        """starts the game"""
+        self.print_board()
         side = 'w'
         count = 0
-        while self.count['b'] > 0 and self.count['w'] > 0:
+        charge = True
+        while self.count['b'] > 0 and self.count['w'] > 0 and charge:
             print('\nBlack: ', self.count['b'])
             print('White: ', self.count['w'])
+            print('Move:', count)
 
             path = self.computer_move(side)
             self.move(path, side)
-            self.draw_board()
+            self.print_board()
 
             side = 'b' if side == 'w' else 'w'
             count += 1
+            charge = (self.parity['w'][0]
+                      and self.parity['b'][0]) or (self.parity['w'][1]
+                                                   and self.parity['b'][1])
 
-        if self.count['b'] == 0:
-            print('White wins! in ', end="")
+        if not charge:
+            print('Game draws :( in ', end='')
+        elif self.count['b'] == 0:
+            print('White wins! in ', end='')
         else:
             print('Black wins! in ', end='')
         print(count, 'moves.')
 
 
-b = Checker()
-b.start()
+game = Checker()
+game.start()
